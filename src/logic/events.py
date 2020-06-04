99e9d6bd6bc991e55406702a9b92ba276e9f7001
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2020-06-04 10:25:46 +0200

    Now captures the keyboard in the window

--- src/logic/events.py
+++ src/logic/events.py
@@ -4,3 +4,8 @@ from tkinter import *
 def save_click_result(chosen):
     print(f'{chosen}')
     # print(f'left is harder')
+
+
+def save_keyboard_result(chosen):
+    print(f'In [save_keyboard_result]: saving results for: {chosen}')
+
--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import gui
 
 
 def main():
-    gui.show_window()
+    # gui.show_window()
+    gui.show_window_with_keyboard_input()
 
 
 if __name__ == '__main__':
