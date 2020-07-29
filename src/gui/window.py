bad4ea84d65e15214481b1655eefa892020b124f
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2020-07-29 11:45:34 +0200

    Update window.py
    UI fix

--- src/gui/window.py
+++ src/gui/window.py
@@ -389,25 +389,27 @@ class Window:
         # ======== final page
         if self.current_index == len(self.cases):
             log(f"\n\n\n{'=' * 150}\nON THE FINAL PAGE", no_time=True)
+            self.left_caption_panel.pack_forget()
+            self.right_caption_panel.pack_forget()
             self.fin_button.pack(side=TOP)  # show finalize button
             self.discard_button.pack_forget()
             self.rate_9_indicator.pack_forget()
             self.rate_1_indicator.pack_forget()
             self.rate_2_indicator.pack_forget()
             self.update_photos_async(rate, 'final', files_already_updated)
-            # self.update_photos(frame='final', files_already_updated=files_already_updated)
 
         # ======== other pages
         if self.current_index != len(self.cases):
             self.compute_case_number()
             log_current_index(self, called_from='update_frame')
+            self.left_caption_panel.pack(side=LEFT)
+            self.right_caption_panel.pack(side=RIGHT)
+            self.fin_button.pack_forget()  # hide finalize button
+            self.discard_button.pack(side=BOTTOM)
             self.rate_9_indicator.pack(side=TOP)
             self.rate_1_indicator.pack(side=TOP)
             self.rate_2_indicator.pack(side=TOP)
             self.update_photos_async(rate, 'others', files_already_updated)
-            # self.update_photos(frame='others', files_already_updated=files_already_updated)
-            self.fin_button.pack_forget()  # hide finalize button
-            self.discard_button.pack(side=BOTTOM)
 
     # ======================================== logical functions  ========================================
     # ====================================================================================================
